e68df42dde07c48607a1a7561e16546cfc1fb7f6
Josephine Mui <josephinemui@yahoo.com>
2016-11-13 18:02:12 -0600

    he

--- MyJavascript.js
+++ MyJavascript.js
@@ -6,23 +6,7 @@ var thing = "pigs";
 apple pie
 Fixed issues
 
-function DoStuff() {
 
-    console.log("hi");
-    console.error("it broke");
-    console.debug("debuggin");
-    console.info("information");
-    console.warn("warning");
-    console.assert(10 == 5 * 3);
-
-  
-
-    console.group("%cFinishing part", "font-size: x-large")
-    LoadBanners();
-    SoundTrumpets();
-    console.groupEnd();
-   
-}
 
 
 
